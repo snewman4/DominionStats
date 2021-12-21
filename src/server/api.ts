6712edc74e8c7ab5958803cd09dbef274ca5cc7d
Matt Buland <gik0geck0@gmail.com>
2021-12-20 20:20:02 -0700

    Remove helmet for default LWR config (#41)
    * Remove helmet for default LWR config
    * Cleanup unused imports

--- src/server/api.ts
+++ src/server/api.ts
@@ -3,10 +3,8 @@ import bodyParser from 'body-parser';
 import compression from 'compression';
 import pgSession from 'connect-pg-simple';
 import cookieParser from 'cookie-parser';
-import express, { NextFunction } from 'express';
+import type { NextFunction } from 'express';
 import expressSession from 'express-session';
-import helmet from 'helmet';
-import path from 'path';
 import passport from 'passport';
 import passportGoogle from 'passport-google-oauth20';
 
@@ -35,18 +33,6 @@ function setupRoutes() {
     if (HOST !== 'localhost') {
         SCHEME = 'https://';
         PUBLIC_PORT = '443';
-        app.use(helmet());
-        app.use(
-            helmet({
-                contentSecurityPolicy: {
-                    useDefaults: true,
-                    directives: {
-                        'upgrade-insecure-requests': null
-                    }
-                },
-                noSniff: undefined
-            })
-        );
     }
 
     const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
