253c4238292f0872a33b5add75ffa334ae03bb2e
snewman4 <90208190+snewman4@users.noreply.github.com>
2022-06-03 14:36:50 -0600

    Rolled back broken DB setup

--- db-migrations/5_log_table.sql
+++ db-migrations/5_log_table.sql
@@ -1,12 +1,8 @@
 CREATE TABLE IF NOT EXISTS log_game_round (
-	id serial PRIMARY KEY,
+    player_turn INT,
+    turn_index INT,
     game_label VARCHAR(255) NOT NULL,
     player_name VARCHAR(255) NOT NULL,
     cards_played JSON NOT NULL,
-    cards_purchased JSON NOT NULL,
-    player_turn INT,
-    turn_index INT,
-    CONSTRAINT
-        FOREIGN KEY(game_label)
-            REFERENCES game_results(game_label)
+    cards_purchased JSON NOT NULL
 )
