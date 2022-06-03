2de2bf745f3df103f24a1d902145d74b990e6b97
Joshua Boerma <jboerma@mines.edu>
2022-06-03 14:21:57 -0600

    Added turns to database

--- db-migrations/5_log_table.sql
+++ db-migrations/5_log_table.sql
@@ -3,7 +3,9 @@ CREATE TABLE IF NOT EXISTS log_game_round (
     game_label VARCHAR(255) NOT NULL,
     player_name VARCHAR(255) NOT NULL,
     cards_played JSON NOT NULL,
-    cards_purchased JSON NOT NULL
+    cards_purchased JSON NOT NULL,
+    player_turn INT,
+    turn_index INT,
     CONSTRAINT
         FOREIGN KEY(game_label)
             REFERENCES game_results(game_label)
