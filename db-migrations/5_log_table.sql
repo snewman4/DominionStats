ee94ec65aea041b7cdf56a611b546cbf5d2f42f8
Joshua Boerma <jboerma@mines.edu>
2022-06-03 14:42:55 -0600

    Added composite key

--- db-migrations/5_log_table.sql
+++ db-migrations/5_log_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS log_game_round (
-    player_turn INT,
-    turn_index INT,
+	id serial PRIMARY KEY,
     game_label VARCHAR(255) NOT NULL,
     player_name VARCHAR(255) NOT NULL,
     cards_played JSON NOT NULL,
-    cards_purchased JSON NOT NULL
+    cards_purchased JSON NOT NULL,
+    player_turn INT,
+    turn_index INT,
+    PRIMARY KEY (id, game_label, player_name)
 )
