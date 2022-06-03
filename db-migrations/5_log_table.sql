673b6c526670068867da97471fdc162a2f5f260c
Joshua Boerma <jboerma@mines.edu>
2022-06-03 14:20:19 -0600

    Added foreign game_label to log_game_round

--- db-migrations/5_log_table.sql
+++ db-migrations/5_log_table.sql
@@ -4,4 +4,7 @@ CREATE TABLE IF NOT EXISTS log_game_round (
     player_name VARCHAR(255) NOT NULL,
     cards_played JSON NOT NULL,
     cards_purchased JSON NOT NULL
+    CONSTRAINT
+        FOREIGN KEY(game_label)
+            REFERENCES game_results(game_label)
 )
