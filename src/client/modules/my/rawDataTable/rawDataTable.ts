85efd44aa9b3d76b6a1ee9853cf63629541bfd89
Racyn Komata <rkomata@mines.edu>
2022-06-13 13:28:26 -0600

    Created new tab on the homepage to display the raw data table

---
+++ src/client/modules/my/rawDataTable/rawDataTable.ts
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import { getRawResults, extractaGameSizePlayerStats } from 'my/resultsFetcher';
+import type { PlayerStatsAllGames } from 'my/resultsFetcher';
+
+export default class sixPlayerRankingTable extends LightningElement {
+    tableData: PlayerStatsAllGames[] = [];
+    async connectedCallback() {
+        this.tableData = extractaGameSizePlayerStats(await getRawResults(), 6);
+    }
+}
